a6125d392251d3afc6217a8cf5adcbda70bbdda3
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-15 17:24:51 +0530

    token types and identifiers for monkey lang

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+//goland:noinspection GoNameStartsWithPackageName
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+//goland:noinspection GoCommentStart
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT" // add, foobar, x, y...
+	INT   = "INT"   // 12345...
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPARAN = "("
+	RPARAN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
