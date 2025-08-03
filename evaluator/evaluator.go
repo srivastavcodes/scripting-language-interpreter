9825b3f992a0abda30868f19812a696ac31c2a88
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-03 20:22:26 +0530

    add boolean evaluation and tests
    Took 27 minutes

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"Interpreter_in_Go/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -16,6 +22,8 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return boolNativeToBoolObject(node.Value)
 	default:
 		return nil
 	}
@@ -28,4 +36,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(stmt)
 	}
 	return result
+}
+
+func boolNativeToBoolObject(value bool) *object.Boolean {
+	if value {
+		return TRUE
+	} else {
+		return FALSE
+	}
 }
