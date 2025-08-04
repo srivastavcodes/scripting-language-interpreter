1f58543b075b1217162a2beac8c4fdd13e33de92
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-04 09:45:13 +0530

    refactor program evaluation to accept *ast.Program instead of []ast.Statement
    Took 7 minutes

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -13,13 +13,13 @@ var (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
-	case *ast.Program:
-		return evalProgram(node.Statements)
-	case *ast.ExpressionStatement:
-		return Eval(node.Expression)
 	case *ast.ReturnStatement:
 		reVal := Eval(node.ReturnValue)
 		return &object.ReturnValue{Value: reVal}
+	case *ast.Program:
+		return evalProgram(node)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -43,10 +43,10 @@ func Eval(node ast.Node) object.Object {
 	}
 }
 
-func evalProgram(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, stmt := range stmts {
+	for _, stmt := range program.Statements {
 		result = Eval(stmt)
 		if returnVal, ok := result.(*object.ReturnValue); ok {
 			return returnVal.Value
