3ed6519b0c48b376854d57464499c9d6a2a8ee75
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-04 08:53:07 +0530

    add if-else expression evaluation and tests
    Took 25 minutes

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -17,6 +17,8 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -30,6 +32,9 @@ func Eval(node ast.Node) object.Object {
 		lt := Eval(node.Left)
 		rt := Eval(node.Right)
 		return evalInfixExpression(node.Operator, lt, rt)
+
+	case *ast.IfExpression:
+		return evalConditionalExpression(node)
 	default:
 		return nil
 	}
@@ -103,6 +108,18 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 	}
 }
 
+func evalConditionalExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
 func evalPrefixNegationExpression(right object.Object) object.Object {
 	if right.Type() != object.INTEGER_OBJ {
 		return NULL
@@ -121,5 +138,18 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
