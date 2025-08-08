d680015e5396652fba25746dfc88f4815924af0a
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-08 09:00:19 +0530

    add built-in functions for array manipulation: first, last, rest, and push
    Took 37 minutes

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -14,8 +14,75 @@ var builtIns = map[string]*object.BuiltIn{
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
-				return createError("argument to `len()` not supported, got %s", args[0].Type())
+				return createError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"first": {
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return createError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return createError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+			if len(array.Elements) > 0 {
+				return array.Elements[0]
+			}
+			return NULL
+		},
+	},
+	"last": {
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return createError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return createError("argument to `last` must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+			if len(array.Elements) > 0 {
+				return array.Elements[len(array.Elements)-1]
+			}
+			return NULL
+		},
+	},
+	"rest": {
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return createError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return createError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+
+			length := len(array.Elements)
+			if len(array.Elements) > 0 {
+				copied := make([]object.Object, length-1)
+				copy(copied, array.Elements[1:length])
+				return &object.Array{Elements: copied}
+			}
+			return NULL
+		},
+	},
+	"push": {
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return createError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return createError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+
+			copied := make([]object.Object, length+1)
+			copy(copied, array.Elements)
+
+			copied[length] = args[1]
+			return &object.Array{Elements: copied}
 		},
 	},
 }
