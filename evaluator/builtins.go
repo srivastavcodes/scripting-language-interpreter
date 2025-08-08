960760139f2f6c03c6a4b9bdba740b13a41c0ba5
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-08 08:23:29 +0530

    added support for arrays
    Took 6 minutes

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -9,10 +9,12 @@ var builtIns = map[string]*object.BuiltIn{
 				return createError("wrong number of arguments. got=%d, want=1", len(args))
 			}
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
-				return createError("argument must be a string, got %s", args[0].Type())
+				return createError("argument to `len()` not supported, got %s", args[0].Type())
 			}
 		},
 	},
